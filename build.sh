19dbf8ff6824f83159609adadd9016947051f700
Charles Andrews <cfandrews@outlook.com>
2023-10-15 17:03:53 -0400

    Remove dev_build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+rm -rf .mypy_cache .pytest_cache .ruff_cache ./src/avltree.egg-info venv .coverage pyvenv.cfg dist
+set -e
+python3 -m venv venv
+./venv/bin/pip install -e '.[dev]'
+./venv/bin/pre-commit install
+./venv/bin/isort .
+./venv/bin/black ./src
+./venv/bin/black ./tests
+./venv/bin/ruff check ./src
+./venv/bin/ruff check ./tests
+./venv/bin/mypy
+./venv/bin/pytest
+./venv/bin/python3 -m build
