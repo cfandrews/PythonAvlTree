7817ac4d499719a11dfe98d8583536a05a767714
Charles Andrews <cfandrews@outlook.com>
2023-10-27 21:27:51 -0400

    Update build dependencies

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 rm -rf .mypy_cache .pytest_cache .ruff_cache ./src/avltree.egg-info venv .coverage pyvenv.cfg dist
 set -e
 python3 -m venv venv
-./venv/bin/pip install -e '.[dev]'
+./venv/bin/pip install -e '.[build]'
 ./venv/bin/pre-commit install
 ./venv/bin/isort .
-./venv/bin/black ./src
-./venv/bin/black ./tests
+./venv/bin/ruff format ./src
+./venv/bin/ruff format ./tests
 ./venv/bin/ruff check ./src
 ./venv/bin/ruff check ./tests
 ./venv/bin/mypy
